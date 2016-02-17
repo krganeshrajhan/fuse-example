c97b91b5aa4c0dfd67700c29e0277bf654f3ed10
krganeshrajhan <krganeshrajhan@gmail.com>
2016-02-17 17:55:14 +0800

    Part 3, Part 4
    Part 3 and Part 4 of HomeLoan demo

---
+++ src/main/java/fuse/example/homeloan/endpoint/SummaryService.java
@@ -0,0 +1,19 @@
+package fuse.example.homeloan.endpoint;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("summaryService")
+public class SummaryService {
+	
+	@GET
+	@Path("/customer/{nationalID}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getSummary(@PathParam("nationalID") String nationalID) {
+		return null;
+	}
+
+}
